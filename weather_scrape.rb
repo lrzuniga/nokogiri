246a158542f9e660a5b49b563e1bdea769581b1f
lrzuniga <lrzuniga@gmail.com>
2013-08-28 17:58:14 -0400

    replace array's with hashes

--- weather_scrape.rb
+++ weather_scrape.rb
@@ -4,25 +4,47 @@ require 'open-uri'
 page = Nokogiri::HTML(open("http://weather.gc.ca/"))
 
 city_left = page.css('td.width-25 a')
-weather_left = page.css('td.width-10.align-right')
+city_left_array = []
+  city_left.each do |city|
+  city_left_array << city.text
+  end
 
 city_right = page.css('td.table-border-left.width-20 a')
-weather_right = page.css('td.align-right')
+city_left_array = []
+  city_left.each do |city|
+  city_left_array << city.text
+  end
 
-city_all = [city_left, weather_left, city_right, weather_right]
+city_right_array = []
+  city_right.each do |city|
+  city_right_array << city.text
+  end
 
-city_hash = 
+city_all = city_left_array.concat(city_right_array)
+
+weather = page.css('td.align-right')
+weather_all = []
+  weather.each do |w|
+  weather_all << w.text
+  end
+
+weather_hash = {}
+  city_all.each_with_index do | city, index |
+  weather_hash[city]=weather_all[index]
+  end
+
 File.open('weather.html', 'w') do |f|
   f.puts("<html>")
   f.puts("<head>")
+  f.puts("<meta charset='UTF-8'>")
   f.puts("<title> Intro to HTML/CSS</title>")
   f.puts("</head>")
   f.puts("<body>")
   f.puts("<h1>Canadian Weather - source: (http://weather.gc.ca)</h1>")
   f.puts("<ul>")
 
-  city_all.each do |city|
-    f.puts("<li>" + city.text + "</li>")
+  weather_hash.each do |city, weather|
+    f.puts ("<li>") + city + " - " + weather + ("</li>")
   end
 
   f.puts("</ul")
